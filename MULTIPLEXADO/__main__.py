41df75689cbbd2d499ab39198665a386cbe49b79
Gerry1402 <gerard.vello@gmail.com>
2024-12-21 23:19:39 +0100

    arreglado el multiplexado

--- MULTIPLEXADO/__main__.py
+++ MULTIPLEXADO/__main__.py
@@ -3,10 +3,13 @@ from CLASSES.mkv import MKV
 from directorios import *
 from .start import *
 
+idiomas_extra = []
+
 for archivo in archivos:
-    mkv = MKV(os.path.join(carpeta_multiplexado, archivo))
+    mkv = MKV(os.path.join(carpeta_multiplexado.ruta, archivo))
     #mkv.idiomas(idiom={}, forz=[])
     #mkv.eliminar(tracks=[])
+    mkv.conservar(idiomas = orden_idiomas + idiomas_extra)
     mkv.renombrar(titulo='', auto=True)
     mkv.reordenar(idiomas=orden_idiomas)
     #mkv.sincronizar(tiempo=350, audios = [], subtitulos = [], forz={})
--- MULTIPLEXADO/start.py
+++ MULTIPLEXADO/start.py
@@ -3,7 +3,7 @@ from CLASSES.mkv import iso6392
 from directorios import *
 
 
-archivos = [archivo for archivo in os.listdir(carpeta_multiplexado) if os.path.splitext(archivo)[-1] == '.mkv']
+archivos = [archivo for archivo in os.listdir(carpeta_multiplexado.ruta) if os.path.splitext(archivo)[-1] == '.mkv']
 
 print(f'Para los idiomas utilizar la etiqueta iso-6392. Los idiomas que hay registrados son {', '.join(iso6392[:-1])} y {iso6392[-1]}\n')
 
