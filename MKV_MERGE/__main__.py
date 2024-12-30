8c3c6e1daffd4639b2e7d4502719c57e626218b3
Gerry1402 <gerard.vello@gmail.com>
2024-12-30 03:57:21 +0100

    Merge Arreglado (creo)

--- MKV_MERGE/__main__.py
+++ MKV_MERGE/__main__.py
@@ -14,26 +14,27 @@ delay_ASS = 0
 
 ################## FUNCIONES AUXILIARES. NO TOCAR.#############################################################
 
-exit("  ¡¡   El numero de videos y de audios no coinciden   !!") if Audio and len(os.listdir(carpeta_merge.audios)) != len(os.listdir(carpeta_merge.videos)) else None
-exit("  ¡¡   El numero de videos y de subtitulos no coinciden   !!") if Subtitulos and len(os.listdir(carpeta_merge.subtitulos)) != len(os.listdir(carpeta_merge.videos)) else None
 
 ###############################################################################################################
 
 if not Audio and not Subtitulos:
     exit("¿¿Pero que quieres que haga Pereira?? Que quieres que haga???")
 
-audios = os.listdir(carpeta_merge.audios)
-subtitulos = os.listdir(carpeta_merge.subtitulos)
-n_totales = len(os.listdir(carpeta_merge.videos))
+audios = [os.path.join(carpeta_merge.audios.done, audio) for audio in os.listdir(carpeta_merge.audios.done)]
+subtitulos = [os.path.join(carpeta_merge.subtitulos.done, subtitulo) for subtitulo in os.listdir(carpeta_merge.subtitulos.done)]
+videos = [os.path.join(carpeta_merge.videos, video) for video in os.listdir(carpeta_merge.videos)]
 
-for i, video in enumerate(carpeta_merge.videos, start=1):
+exit("  ¡¡   El numero de videos y de audios no coinciden   !!") if Audio and len(audios) != len(videos) else None
+exit("  ¡¡   El numero de videos y de subtitulos no coinciden   !!") if Subtitulos and len(subtitulos) != len(videos) else None
+
+for i, video in enumerate(videos, start=1):
     nombre, extension = os.path.splitext(os.path.basename(video))
     os.makedirs(carpeta_merge.resultado, exist_ok=True)
     print(f"Multiplexando episodio {nombre}")
     mkv = mkvf(file_path = video)
 
-    mkv.add_track(mkvt(file_path = os.path.join(carpeta_merge.audios, audios[i]), language = ISO_6392_AAC, sync = delay_AAC))  if Audio else None
-    mkv.add_track(mkvt(file_path = os.path.join(carpeta_merge.subtitulos, subtitulos[i]), language=ISO_6392_ASS, sync = delay_ASS))  if Subtitulos else None
+    mkv.add_track(mkvt(file_path = audios[i], language = ISO_6392_AAC, sync = delay_AAC))  if Audio else None
+    mkv.add_track(mkvt(file_path = subtitulos[i], language=ISO_6392_ASS, sync = delay_ASS))  if Subtitulos else None
     mkv.mux(os.path.join(carpeta_merge.resultado, f'{nombre}{extension}'), silent = True)
-    print(f"Completado{'s' if i!=1 else ''} {i} de {n_totales}           ", end='\r')
+    print(f"Completado{'s' if i!=1 else ''} {i} de {videos}           ", end='\r')
 print("Todos los episodios han sido procesados                                 ")
