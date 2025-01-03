9cdee66a55e7f9b6e02f3fbf84f041cf97f49df0
Gerry1402 <gerard.vello@gmail.com>
2025-01-03 22:30:50 +0100

    arreglado tiempo archivo individuales

--- MULTIPLEXADO/__main__.py
+++ MULTIPLEXADO/__main__.py
@@ -9,15 +9,15 @@ from rich.text import Text
 def main():
 
     console = Console()
-    tiempo = time.time()
+    tiempo_base = time.time()
 
-    def crear_tabla(progreso_total, archivo_actual, avance_actual, tiempo):
+    def crear_tabla(progreso_total, archivo_actual, avance_actual, tiempo_base, tiempo_parcial):
         tabla = Table.grid(expand=True)
         tabla.add_column(ratio=4)
         [tabla.add_column(ratio=1) for i in range(2)]
         tabla.add_row('', Text('Tiempo Restante', justify='center'), Text('Progreso Actual', justify='center'))
-        tabla.add_row('Progreso Total', Text(f'{int((100/progreso_total-1)*tiempo)} segundos', justify='center'),Text(f"{progreso_total:.2f}%", justify='center'))
-        tabla.add_row(f'{archivo_actual}', Text(f'{int((100/avance_actual-1)*tiempo)} segundos', justify='center'), Text(f'{avance_actual}%', justify='center'))
+        tabla.add_row('Progreso Total', Text(f'{int((100/progreso_total-1)*tiempo_base)} segundos', justify='center'),Text(f"{progreso_total:.2f}%", justify='center'))
+        tabla.add_row(f'{archivo_actual}', Text(f'{int((100/avance_actual-1)*tiempo_parcial)} segundos', justify='center'), Text(f'{avance_actual}%', justify='center'))
         return tabla
 
     with Live(console=console) as live:
@@ -25,11 +25,12 @@ def main():
         for archivo in archivos:
             nombre_archivo = os.path.splitext(os.path.basename(archivo))[0]
             tamaño_archivo = os.path.getsize(archivo)
+            tiempo_parcial = time.time()
             for avance in proceso(archivo):
                 porcentaje = tamaño_archivo * (avance / 100)
                 progreso_total = (tamaño_analizado + porcentaje) * 100 / tamaño_total
                 if progreso_total != 0 and avance != 0:
-                    live.update(crear_tabla(progreso_total, nombre_archivo, avance, time.time() - tiempo))
+                    live.update(crear_tabla(progreso_total, nombre_archivo, avance, time.time() - tiempo_base, time.time() - tiempo_parcial))
             tamaño_analizado += os.path.getsize(archivo)
 
 if __name__ == '__main__':
