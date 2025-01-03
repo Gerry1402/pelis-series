b9ea040e1571f359a03487cb7b89c468897ac523
Gerry1402 <gerard.vello@gmail.com>
2025-01-03 23:22:27 +0100

    arreglado tiempos merge

--- MKV_MERGE/__main__.py
+++ MKV_MERGE/__main__.py
@@ -10,15 +10,15 @@ import time
 def main():
 
     console = Console()
-    tiempo = time.time()
+    tiempo_base = time.time()
 
-    def crear_tabla(progreso_total, archivo_actual, avance_actual, tiempo):
+    def crear_tabla(progreso_total, archivo_actual, avance_actual, tiempo_base, tiempo_parcial):
         tabla = Table.grid(expand=True)
         tabla.add_column(ratio=4)
         [tabla.add_column(ratio=1) for i in range(2)]
         tabla.add_row('', Text('Tiempo Restante', justify='center'), Text('Progreso Actual', justify='center'))
-        tabla.add_row('Progreso Total', Text(f'{int((100/progreso_total-1)*tiempo)} segundos', justify='center'),Text(f"{progreso_total:.2f}%", justify='center'))
-        tabla.add_row(f'{archivo_actual}', Text(f'{int((100/avance_actual-1)*tiempo)} segundos', justify='center'), Text(f'{avance_actual}%', justify='center'))
+        tabla.add_row('Progreso Total', Text(f'{int((100/progreso_total-1)*tiempo_base)} segundos', justify='center'),Text(f"{progreso_total:.2f}%", justify='center'))
+        tabla.add_row(f'{archivo_actual}', Text(f'{int((100/avance_actual-1)*tiempo_parcial)} segundos', justify='center'), Text(f'{avance_actual}%', justify='center'))
         return tabla
 
     with Live(console=console) as live:
@@ -30,11 +30,12 @@ def main():
                 tamaño_video += os.path.getsize(audios[i])
             if hacer_subtitulos:
                 tamaño_video += os.path.getsize(subtitulos[i])
+            tiempo_parcial = time.time()
             for avance in proceso(i):
                 porcentaje = tamaño_video * (avance / 100)
                 progreso_total = (tamaño_analizado + porcentaje) * 100 / tamaño_total
                 if progreso_total != 0 and avance != 0:
-                    live.update(crear_tabla(progreso_total, nombre_video, avance, time.time() - tiempo))
+                    live.update(crear_tabla(progreso_total, nombre_video, avance, time.time() - tiempo_base, time.time() - tiempo_parcial))
             tamaño_analizado += tamaño_video
 
 if __name__ == '__main__':
