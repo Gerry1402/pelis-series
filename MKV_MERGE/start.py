ceede6a487c3b809690fd99ebaa7735b51b15f82
Gerry1402 <gerard.vello@gmail.com>
2025-01-03 22:46:46 +0100

    arreglado merge

--- MKV_MERGE/start.py
+++ MKV_MERGE/start.py
@@ -9,24 +9,24 @@ from rich import print
 audios = [os.path.join(carpeta_merge.audios.done, audio) for audio in os.listdir(carpeta_merge.audios.done)]
 subtitulos = [os.path.join(carpeta_merge.subtitulos.done, subtitulo) for subtitulo in os.listdir(carpeta_merge.subtitulos.done)]
 videos = [os.path.join(carpeta_merge.videos, video) for video in os.listdir(carpeta_merge.videos)]
-if len(videos) != len(audios) or len(videos) != len(subtitulos):
-    exit("  ¡¡   El numero de videos y de audios no coinciden   !!")
 n_archivos = len(videos)
 tamaño_videos = sum(os.path.getsize(video) for video in videos)
 tamaño_total = tamaño_videos
-tamaño_audios = sum(os.path.getsize(audio) for audio in audios)
-tamaño_subtitulos = sum(os.path.getsize(subtitulo) for subtitulo in subtitulos)
 
 iso6392 = [info_idiomas[idioma]['iso639-2'] for idioma in idiomas]
 
 audios_subtitulos = MultiSelectMenu(enunciado='Opciones para hacer merge (Seleccionar uno o dos)', opciones=['Audios', 'Subtítulos']).mostrar()
 hacer_audios = 'Audios' in audios_subtitulos
 if hacer_audios:
-    tamaño_total += tamaño_audios
+    if len(audios) != n_archivos:
+        exit("  ¡¡   El numero de videos y de audios no coinciden   !!")
+    tamaño_total += sum(os.path.getsize(audio) for audio in audios)
     idioma_audios = info_idiomas[Menu(enunciado='Seleccionar idioma audios', opciones=idiomas, subtitulos=iso6392, columnas=4, limite=4, nombre_limite='... Más idiomas').mostrar()]['iso639-2']
 hacer_subtitulos = 'Subtítulos' in audios_subtitulos
-if hacer_audios:
-    tamaño_total += tamaño_subtitulos
+if hacer_subtitulos:
+    if len(subtitulos) != n_archivos:
+        exit("  ¡¡   El numero de videos y de subtitulos no coinciden   !!")
+    tamaño_total += sum(os.path.getsize(subtitulo) for subtitulo in subtitulos)
     idioma_subtitulos = info_idiomas[Menu(enunciado='Seleccionar idioma subtitulos', opciones=idiomas, subtitulos=iso6392, columnas=4, limite=4, nombre_limite='... Más idiomas').mostrar()]['iso639-2']
 
 
